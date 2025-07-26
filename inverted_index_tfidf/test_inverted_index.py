be95167c7378b8673bcf1c0d7298359ccdb8e950
wesd <wesdoyle08@gmail.com>
2025-07-26 14:49:53 -0500

    Add empty result test

--- inverted_index_tfidf/test_inverted_index.py
+++ inverted_index_tfidf/test_inverted_index.py
@@ -8,3 +8,10 @@ def test_add_single_document_and_search():
     assert len(results) == 1
     assert results[0].doc_id == "doc1"
     assert results[0].score > 0
+
+def test_search_nonexistent_term_returns_empty():
+    index = InvertedIndex()
+    index.add_document("doc1", "hello world")
+    results = index.search("foo")
+    assert len(results) == 0
+
