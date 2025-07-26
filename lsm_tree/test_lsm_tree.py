a6f021a276447dfa8f5904425259b832350e6781
wesd <wesdoyle08@gmail.com>
2025-07-26 09:13:32 -0500

    Add tests
    Add tests for getting and updating keys from the memtable

--- lsm_tree/test_lsm_tree.py
+++ lsm_tree/test_lsm_tree.py
@@ -6,3 +6,17 @@ def test_put_and_get_single_item():
     val = "val"
     lsm.put(key, val)
     assert lsm.get(key) == val
+
+def test_get_nonexistent_key_returns_none():
+    lsm = LSMTree()
+    key = "foo"
+    assert lsm.get(key) is None
+
+def test_update_existing_key_overwrites_old_value():
+    lsm = LSMTree()
+    key = "foo"
+    lsm.put(key, "hello")
+    lsm.put(key, "world")
+    val = lsm.get(key)
+    assert val == "world"
+
