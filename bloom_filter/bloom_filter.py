5a1a34f5fc12892d259a680be89bfbdbdb84ef41
wesd <wesdoyle08@gmail.com>
2025-07-26 21:41:59 -0500

    Add expected_items and false_pos_rate params

--- bloom_filter/bloom_filter.py
+++ bloom_filter/bloom_filter.py
@@ -1,6 +1,8 @@
 class BloomFilter:
 
-    def __init__(self):
+    def __init__(self, expected_items: int = 1_000, false_pos_rate=0.01):
+        self._expeted_items = expected_items
+        self._false_pos_rate = false_pos_rate
         self._items = set()
 
     def contains(self, key: str):
--- bloom_filter/test_bloom_filter.py
+++ bloom_filter/test_bloom_filter.py
@@ -10,3 +10,31 @@ def test_contains_gets_added_item():
     bf.add("foo")
     assert bf.contains("foo")
     assert not bf.contains("bar")
+
+def test_no_false_negatives():
+    bf = BloomFilter()
+    items = [f"item:{i}" for i in range(1_000_000)]
+
+    for item in items:
+        bf.add(item)
+
+    for item in items:
+        assert bf.contains(item)
+
+def test_can_create_with_expected_items_param():
+    bf = BloomFilter(expected_items=1_000)
+    assert bf is not None
+    bf.add("x")
+    assert bf.contains("x")
+
+def test_can_create_with_false_positives_param():
+    bf = BloomFilter(expected_items=256, false_pos_rate=0.001)
+    assert bf is not None
+    bf.add("x")
+    assert bf.contains("x")
+
+def test_uses_bit_array():
+    bf = BloomFilter(expected_items=256, false_pos_rate=0.001)
+    bf.add("foo")
+    assert hasattr(bf, "bit_words")
+    assert bf.contains("foo")
