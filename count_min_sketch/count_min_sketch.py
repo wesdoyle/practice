6ed370ae6501d9f673a89ce1de459ca2a4f4944b
wesd <wesdoyle08@gmail.com>
2025-07-27 21:44:16 -0500

    Start frequency test

--- count_min_sketch/count_min_sketch.py
+++ count_min_sketch/count_min_sketch.py
@@ -1,3 +1,15 @@
 class CountMinSketch:
+    """
+    Count-Min Sketch probabilistic frequency estimator.
+    
+    SPEC:
+    - Estimates frequency of items in a stream using fixed memory
+    - Guarantees no underestimation (one-sided error)
+    - Configurable accuracy vs memory trade-off
+    - Uses multiple hash functions and takes minimum across rows
+    """
     def __init__(self):
         pass
+
+    def frequency(self, key: str):
+        return 0
--- count_min_sketch/test_count_min_sketch.py
+++ count_min_sketch/test_count_min_sketch.py
@@ -3,3 +3,8 @@ from .count_min_sketch import CountMinSketch
 def test_can_create_count_min_sketch():
     cms = CountMinSketch()
     assert cms is not None
+
+def test_frequency_of_unseen_item_is_zero():
+    cms = CountMinSketch()
+    assert cms.frequency("foo") == 0
+    assert cms.frequency("bar") == 0
