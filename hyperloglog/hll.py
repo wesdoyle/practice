e5308fb037566f06125881becd38afe964542617
wesd <wesdoyle08@gmail.com>
2025-07-27 19:15:16 -0500

    Add basic spec

--- hyperloglog/hll.py
+++ hyperloglog/hll.py
@@ -1,3 +1,12 @@
 class HyperLogLog:
+    """
+    HyperLogLog probabilistic cardinality estimator.
+    
+    SPEC:
+    - Estimates the number of unique items in a stream
+    - Uses fixed memory regardless of actual cardinality
+    - Provides configurable accuracy vs memory trade-off
+    - Uses hash functions and statistical properties of bit patterns
+    """
     def __init__(self):
         pass
--- hyperloglog/test_hll.py
+++ hyperloglog/test_hll.py
@@ -2,3 +2,4 @@ from .hll import HyperLogLog
 
 def test_can_create_hyperloglog():
     hll = HyperLogLog()
+    assert hll is not None
